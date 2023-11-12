d3fe1d64fc20b41aecc677a15f3e25ecc16ba400
ion2266 <ionmocanu2266@gmail.com>
2023-11-13 01:24:44 +0200

    changed webconfig to allow compile ts files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/index.ts",
     output: {
         path: path.join(__dirname, "public"),
     },
@@ -13,6 +13,20 @@ module.exports = {
                 use: {
                     // `.swcrc` can be used to configure swc
                     loader: "swc-loader",
+                },
+            },
+            {
+                test: /\.ts$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "swc-loader",
+                    options: {
+                        jsc: {
+                            parser: {
+                                syntax: "typescript",
+                            },
+                        },
+                    },
                 },
             },
         ],
